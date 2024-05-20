26748bfeb53af729ebd857f3d94556d5320fdc32
Vladyslav Usatenko <245127@edu.p.lodz.pl>
2024-05-20 20:35:39 +0200

    10. Wygenerowania getter w klasie DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/CountingOut.java
+++ src/main/java/edu/kis/vh/nursery/CountingOut.java
@@ -10,6 +10,14 @@ public class CountingOut {
 
 	private int total = -1;
 
+	public int getTotal() {
+		return total;
+	}
+
+	public void setTotal(int total) {
+		this.total = total;
+	}
+
 	public void countIn(int in) {
 		if (!isFull())
 			numbers[++total] = in;

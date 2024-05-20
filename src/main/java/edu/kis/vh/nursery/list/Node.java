fd65a326e9f2f6999d031e0fd6f27ecf813f1cd3
Vladyslav Usatenko <245127@edu.p.lodz.pl>
2024-05-20 20:42:56 +0200

    11. Dokonano hermetyzacji nieprywatnych atrybutow

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,11 +2,32 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    protected final int value;
-    protected Node prev, next;
+    private final int value;
+    private Node prev;
+	private Node next;
 
     public Node(int i) {
         value = i;
     }
+
+	protected int getValue() {
+		return value;
+	}
+
+	protected Node getNext() {
+		return next;
+	}
+
+	protected void setNext(Node next) {
+		this.next = next;
+	}
+
+	protected Node getPrev() {
+		return prev;
+	}
+
+	protected void setPrev(Node prev) {
+		this.prev = prev;
+	}
     
 }
